16dd77d1d7aacf4388bed3958b3954499caa1ea6
DagnachewA <wchegnada41@gmail.com>
2023-09-10 01:09:32 -0700

    print combination digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,34 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Dagnachew Amare Menlargilih
+ */
+
 #include <stdio.h>
+
 /**
-* main - prints a mix of single digit numbers in order,comma and space gap
-*
-* Return: Always 0 (success)
-*/
+ * main - prints a mix of single digit numbers in order, comma, and space gap
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int i, j;
+	int n1, n2;
 
-	putchar('0');
-	putchar('1');
-
-	for (i = 0; i <= 8; i++)
+	for (n1 = 0; n1 <= 8; n1++)
 	{
-		for (j = i + 1; j <= 9; j++)
-		 {
-			 putchar(',');
-			 putchar(' ');
-			 putchar(i + '0');
-			 putchar(j + '0');
-		 }
+	for (n2 = n1 + 1; n2 <= 9; n2++)
+	{
+	putchar(n1 + '0');
+	putchar(n2 + '0');
+
+	if (n1 == 8 && n2 == 9)
+		continue;
+
+	putchar(',');
+	putchar(' ');
 	}
+	}
+
 	putchar('\n');
 	return (0);
 }
