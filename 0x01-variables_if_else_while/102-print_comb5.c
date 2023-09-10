5957a857495b6f7b24dc0e361d29a5d050883244
DagnachewA <wchegnada41@gmail.com>
2023-09-10 02:31:40 -0700

    Combination of numbers ranging 0-99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+/*
+ * File name: 102-print_comb5.c
+ * Coder: Dagnachew Amare Menlargilih
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two numbers ranging 0-99
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+			putchar(',');
+			if (n1 == 98 && n2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
