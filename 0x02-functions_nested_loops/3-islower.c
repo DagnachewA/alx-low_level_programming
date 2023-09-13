c40c9a82f7562e04d5d5f6ffbc4b65a92fe30bd3
DagnachewA <wchegnada41@gmail.com>
2023-09-13 12:04:07 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/*
+ * File Name: 3-islower.c
+ * Auth: Dagnachew Amare Menlargilih
+ */
+#include "main.h"
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
