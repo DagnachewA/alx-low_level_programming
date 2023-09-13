f2cffef38fd06b0350956f67b0183daea484c4d5
DagnachewA <wchegnada41@gmail.com>
2023-09-13 13:08:15 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+/*
+ * File Name: 9-times_table.c
+ * Coder: Dagnachew Amare Menlargilih
+ */
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int num, mul, res;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			res = num * mul;
+
+			if (res <= 9)
+				_putchar(' ');
+			else
+				_putchar((res / 10) + '0');
+
+			_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
