cfc0afb299b9ca51ef098dd90530a6f495b8bd9d
DagnachewA <wchegnada41@gmail.com>
2023-09-13 11:35:10 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Dagnachew Amare
+ */
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+		_putchar(let);
+
+	_putchar('\n');
+}
