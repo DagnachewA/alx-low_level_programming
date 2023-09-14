222ed9a74999b48442e4a8bac89f097ee8c6e058
DagnachewA <wchegnada41@gmail.com>
2023-09-14 15:38:23 -0700

    nn

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,17 @@
  * File Name: 10-add.c
  * Coder: Dagnachew Amare Menlargilih
  */
+
 #include "main.h"
+
 /**
- * The function Adds two integers, n1 and n2
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
  *
  * Return: The result of the addition.
  */
-int add(int n1, int n2)
+int add(int num1, int num2)
 {
-	return (n1 + n2);
+	return (num1 + num2);
 }
