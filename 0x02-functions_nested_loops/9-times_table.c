e3a6b41baa501cfadfb0be9ac87e7eeeff5886af
DagnachewA <wchegnada41@gmail.com>
2023-09-14 15:26:49 -0700

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,8 +3,9 @@
  * Coder: Dagnachew Amare Menlargilih
  */
 #include "main.h"
+
 /**
- * The function Prints the 9 times table, starting with 0.
+ * times_table Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
