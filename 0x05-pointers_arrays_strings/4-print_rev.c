3ebade251a49eb8d7ac75b203e2c1eb0ada574cc
DagnachewA <wchegnada41@gmail.com>
2023-09-18 15:21:55 -0700

    array string pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,17 +11,16 @@
  */
 void print_rev(char *s)
 {
+	int i,length = 0;
+
 	if (s == NULL)
 		return;
-	int length = 0;
-	while (s[length] != '\0')
-	{
+
+	while(s[length] != '\0')
 		length++;
-	}
-	int i;
+
 	for (i = length - 1; i >= 0; i--)
-	{
 		putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
