57d8ee7209117b62a6cb39c3466f58d455ba4ba8
DagnachewA <wchegnada41@gmail.com>
2023-09-20 21:55:54 -0700

    Array String pointer more

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,17 +4,22 @@
  */
 
 #include "main.h"
-#include <stddef.h>
+#include <stddef>
 
 /**
- * strcat - Concatenates the string pointed to by @src including
- * the terminating null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
  *
- * Return: A pointer to the destination string @dest.
+ * Description: This function appends the source string to the destination
+ * string, overwriting the terminating null byte ('\0') at the end of dest,
+ * and then adds a terminating null byte. The resulting concatenated string
+ * is stored in the destination string @dest. It assumes that @dest has enough
+ * space to accommodate the additional characters from @src.
+ *
+ * Return: Pointer to the resulting string @dest.
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)/* _strcat */
 {
 	char *dest_ptr = dest;
 
