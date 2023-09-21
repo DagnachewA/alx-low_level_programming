a465266035d1416351e6b1cea2acc070284ffac2
DagnachewA <wchegnada41@gmail.com>
2023-09-20 22:28:28 -0700

    Array Pointer String

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,35 +7,30 @@
 #include <stddef>
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
+ * _strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ ** Description:
+ *   This function appends the string pointed to by @src to the end of the string
+ *   pointed to by @dest. It overwrites the null byte ('\0') at the end of @dest
+ *   and then adds a terminating null byte at the end of the concatenated string.
+ *   The resulting concatenated string is stored in the memory location pointed
+ *   to by @dest. It is assumed that @dest has enough space to accommodate the
+ *   additional characters from @src.
  *
- * Description: This function appends the source string to the destination
- * string, overwriting the terminating null byte ('\0') at the end of dest,
- * and then adds a terminating null byte. The resulting concatenated string
- * is stored in the destination string @dest. It assumes that @dest has enough
- * space to accommodate the additional characters from @src.
- *
- * Return: Pointer to the resulting string @dest.
+ * Return:
+ *   Returns a pointer to the destination string @dest.
  */
-char *_strcat(char *dest, char *src)/* _strcat */
+char *_strcat(char *dest, const char *src)
 {
-	char *dest_ptr = dest;
+	int index = 0, dest_len = 0;
 
-	if (dest == NULL || src == NULL)
-	return (NULL);
+	while (dest[index++])
+		dest_len++;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (dest_ptr);
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
