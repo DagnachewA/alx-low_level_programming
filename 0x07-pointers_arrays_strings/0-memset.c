d7ec44372794070364e970b4b3456e18ed936793
DagnachewA <wchegnada41@gmail.com>
2023-09-26 04:15:53 -0700

    Align implementation of memset with standard library prototype

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 /*
  * File Name: 0-memset.c
  * Coder: Dagnachew Amare
- * Desc: Custom implementation of the memset function in C.
  */
+
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
+ *_memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
